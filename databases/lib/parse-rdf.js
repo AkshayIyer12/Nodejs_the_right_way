a413a588fc1ef369f252f671c79bd0349bbd74ad
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-30 15:41:57 +0530

    Add links like 132 project gutenberg

--- databases/lib/parse-rdf.js
+++ databases/lib/parse-rdf.js
@@ -2,7 +2,7 @@
 const cheerio = require('cheerio');
 module.exports = rdf => {
   const $ = cheerio.load(rdf);
-  const book = {};
+  // const book = {};
   // book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
   // book.title = $('dcterms\\:title').text();
   // book.authors = $('pgterms\\:agent pgterms\\:name')
@@ -13,10 +13,28 @@ module.exports = rdf => {
   //                   console.log('YOOOOOOO ', $(elem).text())
   //                   return $(elem).text()
   //                 });
-  book.subjects = $('[rdf\\:resource$="/LCC"]')
-                  .parent().find('rdf\\:value')
-                  .toArray().map(elem => {
-                    return $(elem).text()
-                  })[0];
-  return book;
+
+  // book.subjects = $('[rdf\\:resource$="/LCC"]')
+  //                 .parent().find('rdf\\:value')
+  //                 .toArray().map(elem => {
+  //                   return $(elem).text()
+  //                 })[0];
+
+  let arr = $('pgterms\\:file').toArray().map(elem => {
+    return elem.attribs['rdf:about']
+  })
+
+  let books = arr.reduce((a, v) => {
+    let val = v.split('/')
+    let attr = val[val.length - 1].split('.').map((elem, i) => {
+      if (i === 0) return elem
+      elem = elem.split('')
+      elem[0] = elem[0].toUpperCase()
+      elem = elem.join('')
+      return elem
+    }).join('')
+    a[attr] = v
+    return a
+  }, {})
+  return books;
 };

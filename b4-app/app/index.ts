fc1abc4634f73b64ced868c7d6de09dea1d98e33
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-02 14:51:12 +0530

    Add showView function

--- b4-app/app/index.ts
+++ b4-app/app/index.ts
@@ -6,8 +6,15 @@ document.body.innerHTML = templates.main();
 const mainElement = document.body.querySelector('.b4-main');
 const alertsElement = document.body.querySelector('.b4-alerts');
 
-mainElement.innerHTML = templates.welcome();
-alertsElement.innerHTML = templates.alert({
-    type: 'info',
-    message: 'Handlebars is working'
-});
+const showView = async () => {
+    const [view, ...params] = window.location.hash.split('/');
+    switch (view) {
+    case '#welcome': mainElement.innerHTML = templates.welcome();
+	break;
+    default: throw Error(`Unrecognized view: ${view}`);
+    }
+};
+
+window.addEventListener('hashchange', showView);
+showView().catch(err => window.location.hash = '#welcome');
+	

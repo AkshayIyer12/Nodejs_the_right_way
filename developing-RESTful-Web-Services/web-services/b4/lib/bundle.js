efc87724ff4525c49f7e5e3d0ffdc39ae5fe496b
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-02 07:34:55 +0530

    Put books in bundle

--- developing-RESTful-Web-Services/web-services/b4/lib/bundle.js
+++ developing-RESTful-Web-Services/web-services/b4/lib/bundle.js
@@ -31,7 +31,36 @@ module.exports = (app, es) => {
       const esResBody = await rp.put({url: bundleUrl, body: bundle, json: true});
       res.status(200).json(esResBody);
     } catch (esResErr) {
-      res.status(esResErr.statusCode || 502).json(esResErr.error)
+      res.status(esResErr.statusCode || 502).json(esResErr.error);
+    }
+  });
+  app.put('/api/bundle/:id/book/:pgid', async (req, res) => {
+    const bundleUrl = `${url}/${req.params.id}`;
+    const bookUrl = `http://${es.host}:${es.port}` + `/${es.books_index}/book/${req.params.pgid}`;
+    try {
+      const [bundleRes, bookRes] = await Promise.all([
+        rp({url: bundleUrl, json: true}),
+        rp({url: bookUrl, json: true})
+      ]);
+      const {_source: bundle, _version: version} = bundleRes;
+      const {_source: book} = bookRes;
+
+      const idx = bundle.books.findIndex(book => book.id === req.params.pgid)
+      if (idx === -1) {
+        bundle.books.push({
+          id: req.params.pgid,
+          title: book.title
+        });
+      }
+      const esResBody = await rp.put({
+        url: bundleUrl,
+        qs: { version },
+        body: bundle,
+        json: true
+      });
+      res.status(200).json(esResBody);
+    } catch (esResErr) {
+      res.status(esResErr.statusCode || 502).json(esResErr.error);
     }
   });
 };
